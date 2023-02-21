065429356dadd0a75493a6be79eb6875007425d6
PholoTC <tebohopholo.tp@gmail.com>
2023-02-21 08:58:11 +0200

    Design the form

--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace Eskom__Prepaid_Electricity
 {
-    public partial class Form1 : Form
+    public partial class frmEskom : Form
     {
-        public Form1()
+        public frmEskom()
         {
             InitializeComponent();
+        }
+
+        private void frmEskom_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
